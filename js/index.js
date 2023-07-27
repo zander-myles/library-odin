f012f7977c1128f0956f01265f5726c3636c426c
Zander Sacay <zandersacay@gmail.com>
2023-07-27 14:09:51 +0800

    Added event listener to exit button to hide new book form

--- js/index.js
+++ js/index.js
@@ -1,7 +1,16 @@
 const newButton = document.querySelector(".new-button");
 const formContainer = document.querySelector(".form-container");
+const exitButton = document.querySelector(".exit-button");
 
 newButton.addEventListener("click", () => {
+    if (formContainer.style.visibility === "hidden") {
+        formContainer.style.visibility = "visible";
+    } else {
+        formContainer.style.visibility = "hidden";
+    }
+})
+
+exitButton.addEventListener("click", () => {
     if (formContainer.style.visibility === "hidden") {
         formContainer.style.visibility = "visible";
     } else {
